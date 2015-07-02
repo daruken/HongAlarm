a08c98a3df4b2ad9462e6c842174c2c7204fa6b8
daruken <dj.hongten@gmail.com>
2015-07-02 21:52:58 +0900

    New source

--- HongAlarm/ViewController.swift
+++ HongAlarm/ViewController.swift
@@ -10,11 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myLabel: UILabel!
+    @IBOutlet weak var mySwitch: UISwitch!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        myLabel.text = "알람"
     }
 
+    @IBAction func mySwitchTapped(sender: AnyObject) {
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
